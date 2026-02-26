c95448116c65f5847132c0d6b88b1e78addd41d6
Toshiaki Okayama Filho <okayamafilho@gmail.com>
2026-02-26 12:01:58 -0300

    create ShippingService class

---
+++ src/main/java/br/com/okayamafilho/desafiospringinjection/services/ShippingService.java
@@ -0,0 +1,19 @@
+package br.com.okayamafilho.desafiospringinjection.services;
+
+import br.com.okayamafilho.desafiospringinjection.entities.Order;
+
+public class ShippingService {
+
+    public double shipment(Order order) {
+        double valorFrete = 0.0;
+        if (order.getBasic() < 100.0) {
+            valorFrete =  20.0;
+        } else if (order.getBasic() >= 100.0 && order.getBasic() <= 200.0) {
+            valorFrete =  12.0;
+        } else if (order.getBasic() >= 200.0) {
+            valorFrete = 0.0;
+        }
+
+        return valorFrete;
+    }
+}
